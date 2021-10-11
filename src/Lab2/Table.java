9ad84fa2c0a040f49ac01a6aa3dac7f9937eb5d9
Trif Diana <59911747+DianaTrif00@users.noreply.github.com>
2021-10-11 19:30:44 +0300

    Initial commit

---
+++ src/Lab2/Table.java
@@ -0,0 +1,14 @@
+package Lab2;
+
+
+public class Table implements Element{
+    private String title;
+
+    public Table(String title) {
+        this.title = title;
+    }
+    public void print()
+    {
+        System.out.println(title);
+    }
+}
---
+++ src/Lab2/TableOfContents.java
@@ -0,0 +1,9 @@
+package Lab2;
+
+public class TableOfContents {
+    public void print()
+    {
+        System.out.println("Mesaj");
+    }
+}
+
